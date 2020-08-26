e4481bcc6c0c53f4507ace9709e7043c8dca74d3
Arunabha Patra <47187868+apatra9@users.noreply.github.com>
2020-08-26 11:26:53 +0530

    added more extensive testing

--- twit/main_test.go
+++ twit/main_test.go
@@ -2,31 +2,56 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"reflect"
 	"testing"
+	"twit/twit"
 
 	"github.com/ChimeraCoder/anaconda"
 )
 
 func TestGothrutweets(t *testing.T) {
 
-	anaconda.SetConsumerKey("CRYLw41CwCqaJyUZIUvYpUcLX")
-	anaconda.SetConsumerSecret("quuoBQZCNtYgs5ieLb6QRJgW72Ck8vsuvCZY6V0NeFEmXD472s")
+	anaconda.SetConsumerKey(os.Getenv("CONSUMERKEY"))
+	anaconda.SetConsumerSecret("CONSUMERSECRET")
 
 	api := anaconda.NewTwitterApi(
-		"1295387221600526337-rehqa8Aj7zH7iHnSQV87o3e0hxZIk0",
-		"s0B62DLqKeyUtZqLcKSMnyG1j7EpY0UGkvmBSE0k2PN6p",
+		"APPKEY",
+		"APPSECRET",
 	)
-	tweet, _ := api.GetTweet(1295746102654713856, nil) //1295746102654713856
+	tweet, _ := api.GetTweet(1295746102654713856, nil) //1295746102654713856, valid tweet
+
 	var expectedReturn []anaconda.Tweet
-	retweets := Gothrutweets(tweet, api)
+
+	tweet, _ = api.GetTweet(2295746102654713856, nil) //2295746102654713856, invalid tweet
+
+	retweets := twit.Gothrutweets(tweet, api)
 
 	//fmt.Println(reflect.ValueOf(retweets).Kind())
 	if reflect.TypeOf(retweets) != reflect.TypeOf(expectedReturn) {
 		t.Error("Wrong type returned.")
 	}
 	if len(retweets) == 0 {
-		t.Error("Couldn't get any retweets! :(")
+		t.Error("Couldn't get any retweets! :(") //add happy test cases and sad test cases separately
+
+	}
+
+	tweet, _ = api.GetTweet(2295746102654713856, nil) //2295746102654713856, invalid tweet
+	if reflect.TypeOf(retweets) != reflect.TypeOf(expectedReturn) {
+		t.Error("Wrong type returned.")
+	}
+	if len(retweets) == 0 {
+		t.Error("Couldn't get any retweets! :(") //add happy test cases and sad test cases separately
+
+	}
+
+	tweet, _ = api.GetTweet(-2, nil) //negative, invalid tweet
+
+	if reflect.TypeOf(retweets) != reflect.TypeOf(expectedReturn) {
+		t.Error("Wrong type returned.")
+	}
+	if len(retweets) == 0 {
+		t.Error("Couldn't get any retweets! :(") //add happy test cases and sad test cases separately
 
 	}
 
@@ -34,13 +59,30 @@ func TestGothrutweets(t *testing.T) {
 
 func TestWin(t *testing.T) {
 
-	ans := Win("i_kanganaranaut", "8", "8")
+	ans := twit.Win("i_kanganaranaut", "8", "8") //good id
 	fmt.Println(len(ans))
 	if len(ans) < 1 {
 		t.Error("Didn't get a valid winner.")
 	}
 
-	if reflect.TypeOf(ans) != reflect.TypeOf(5) {
+	if reflect.TypeOf(ans) != reflect.TypeOf(5) { //sad test case
+		t.Error("Wrong type returned.")
+	}
+
+	if reflect.TypeOf(ans) != reflect.TypeOf("abc") { //happy test case
+		t.Error("Wrong type returned.")
+	}
+	ans = twit.Win("i_kanganakaraut", "8", "8") //bad id
+
+	if len(ans) < 1 {
+		t.Error("Didn't get a valid winner.")
+	}
+
+	if reflect.TypeOf(ans) != reflect.TypeOf(5) { //sad test case
+		t.Error("Wrong type returned.")
+	}
+
+	if reflect.TypeOf(ans) != reflect.TypeOf("abc") { //happy test case
 		t.Error("Wrong type returned.")
 	}
 
